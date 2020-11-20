882de6f0715463ebecef5584998b9e36b2cedf1f
Daniel.Yu <adam741963@gmail.com>
2020-11-19 16:33:58 -0800

    page skeletons

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <div className="App w-100 h-100">
+      <nav className=' container border border-primary m-1 p-1 mx-auto' style={{width: '100vw'}}>
+        Nav bar
+      </nav>
+      <div className=' container border border-primary m-1 p-1 mx-auto' style={{width: '100vw'}}>
+        <form>
+          <div>search new book from</div>
+          <input></input>
+          <button type='submit'>Serach Book</button>
+        </form>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <div id='searchResult' className='container border border-primary mx-auto' style={{width: '100vw'}}>
+        <div>
+          Search Result Container
+        </div>
+        <div className='row border border-primary m-1 p-1'>
+          <ul className='col-11 border border-primary m-1 p-1 mx-auto'>
+            Result
+          </ul>
+        </div>
+      </div>
+      <div id='saveList' className='container border border-primary mx-auto mt-2' style={{width: '100vw'}}>
+        <div>
+          Save List
+        </div>
+        <div className='row border border-primary m-1 p-1'>
+          <ul className='col-11 border border-primary m-1 p-1 mx-auto'>
+            Save List
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
